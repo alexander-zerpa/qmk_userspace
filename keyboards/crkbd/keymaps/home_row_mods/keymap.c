af7cb0410ac2ba0b52ec713c020d631a51f5c3d7
Alexander Zerpa <alexzerpawanner+tech@gmail.com>
2025-04-30 11:22:26 +0200

    feat(keyboards.crkbd.keymaps.home_row_mods): exlude `-` form CAPS WORD

--- keyboards/crkbd/keymaps/home_row_mods/keymap.c
+++ keyboards/crkbd/keymaps/home_row_mods/keymap.c
@@ -118,6 +118,28 @@ uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
 }
 #endif
 
+#ifdef CAPS_WORD_ENABLE
+bool caps_word_press_user(uint16_t keycode) {
+    switch (keycode) {
+        // Keycodes that continue Caps Word, with shift applied.
+        case KC_A ... KC_Z:
+            add_weak_mods(MOD_BIT(KC_LSFT));  // Apply shift to next key.
+            return true;
+
+        // Keycodes that continue Caps Word, without shifting.
+        case KC_1 ... KC_0:
+        case KC_BSPC:
+        case KC_DEL:
+        case KC_MINS:
+        case KC_UNDS:
+            return true;
+
+        default:
+            return false;  // Deactivate Caps Word.
+    }
+}
+#endif
+
 #ifdef OLED_ENABLE
 oled_rotation_t oled_init_user(oled_rotation_t rotation) {
     if (!is_keyboard_master()) return OLED_ROTATION_180;
