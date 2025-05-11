da05784fa05d9a1c4529d093e987ff5d574f1d64
Alexander Zerpa <alexzerpawanner+tech@gmail.com>
2025-05-11 23:52:05 +0200

    feat(keyboards.crkbd.keymaps.home_row_mods): add HR_TOGG key

--- keyboards/crkbd/keymaps/home_row_mods/keymap.c
+++ keyboards/crkbd/keymaps/home_row_mods/keymap.c
@@ -110,7 +110,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       KC_BRID, KC_MPRV, KC_MSTP, KC_MPLY, KC_VOLD, KC_MNXT,                        KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F11,  KC_F12,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_CAPS,  KC_NUM, KC_SCRL, XXXXXXX, KC_MUTE, XXXXXXX,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,TG(CONFIG),
+      KC_CAPS,  KC_NUM, KC_SCRL, XXXXXXX, KC_MUTE, XXXXXXX,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, HR_TOGG,TG(CONFIG),
   //|--------+--------+--------+--------+--------+--------+--------.  ,--------+--------+--------+--------+--------+--------+--------|
                                           _______, _______, _______,    _______, _______, _______
                                       //`--------------------------'  `--------------------------'
